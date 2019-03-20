aabafa36cd57424fbf28a236937b2f46d6735820
Awoh Negheie <awohnegheie@gmail.com>
2019-03-20 17:53:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(string) {
-  return string.toLowerCase("I can't hear you!")
+function sayHiToGrandma("I can't hear you!") {
+  return string.toLowerCase()
 }
